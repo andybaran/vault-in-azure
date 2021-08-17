f9249a7a250f8fbade5a95ef9b236be49264c919
Andy Baran <andy.baran@hashicorp.com>
2021-08-17 13:16:51 -0400

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,26 +1,16 @@
 ############ Outputs
 
-/*
-output "private-key-pem" {
-  value       = tls_private_key.ssh-key.private_key_pem
-  description = "Private key in PEM format"
+output "azurerm_rg_name" {
+  value = azurerm_resource_group.vault-rg.name
   sensitive = false
 }
 
-output "openssh-public-key" {
-  value       = tls_private_key.ssh-key.public_key_openssh
-  description = "OpenSSH Public Key"
+output "azurerm_rg_location" {
+  value = azurerm_resource_group.vault-rg.location
   sensitive = false
 }
 
-output "public-key-md5" {
-  value       = tls_private_key.ssh-key.public_key_fingerprint_md5
-  description = "Public key MD5"
-  sensitive = false
+output "azurerm_subnet" {
+  value = azurerm_subnet.vault-subnet.id
+  senssitive = false 
 }
-
-output "key_vault_name" {
-  value = azurerm_key_vault.vault-vault.name
-  sensitive = false
-}
-*/
