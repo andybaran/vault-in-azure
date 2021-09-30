b2752e77f4226c79985f4699dfd0ea3b921c712e
Andy Baran <andy.baran@hashicorp.com>
2021-09-30 08:24:00 -0400

    need base64

--- main.tf
+++ main.tf
@@ -153,7 +153,7 @@ resource "azurerm_windows_virtual_machine" "windows-vm" {
   admin_username = var.admin_username
   admin_password = var.admin_password
   network_interface_ids = [ azurerm_network_interface.windows-nic.id ]
-  custom_data    = "${local.custom_data_content}"
+  custom_data    = base64(local.custom_data_content)
 
   os_disk {
     caching              = "ReadWrite"
