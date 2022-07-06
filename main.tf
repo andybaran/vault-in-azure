da2689f2b0fd6f5f284398fb9f66b0e5f1eeb020
Andy Baran <andy.baran@hashicorp.com>
2022-07-06 05:32:45 -0400

    update azurem provider on main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.46.0"
-
+      #version = "~> 2.46.0"
+      version = "~>3.12.0"
     }
   }
 }
