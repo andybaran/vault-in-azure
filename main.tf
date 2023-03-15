738eb8be2e48f5bbd8f0249118bbe7b4422598c8
Andy Baran <andy.baran@hashicorp.com>
2023-03-15 11:26:22 -0500

    add log_analytics

--- main.tf
+++ main.tf
@@ -20,6 +20,30 @@ resource "azurerm_resource_group" "vault-rg" {
   tags = var.common-azure-tags
 }
 
+
+#Azure Log Analytics/Sentinel
+
+resource "azurerm_log_analytics_workspace" "log-analytics-workspace" {
+  name                = "la-example-utv-weu"
+  location            = azurerm_resource_group.vault-rg.location
+  resource_group_name = azurerm_resource_group.vault-rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 90
+}
+
+resource "azurerm_log_analytics_solution" "la-opf-solution-sentinel" {
+  solution_name         = "SecurityInsights"
+  location              = azurerm_resource_group.vault-rg.location
+  resource_group_name   = azurerm_resource_group.vault-rg.name
+  workspace_resource_id = azurerm_log_analytics_workspace.log-analytics-workspace.id
+  workspace_name        = azurerm_log_analytics_workspace.log-analytics-workspace.name
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/SecurityInsights"
+  }
+}
+
+
 #data "azurerm_client_config" "current" {}
 
 #  Virtual Machines
